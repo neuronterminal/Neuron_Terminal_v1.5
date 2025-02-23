f1aa7d7912c2af061e488b834ef2c2076c91c2d2
Jay <netneuron1@gmail.com>
2025-02-22 19:17:29 -0500

    Update client.ts

--- src/utils/twitter/client.ts
+++ src/utils/twitter/client.ts
@@ -1,10 +1,10 @@
 import { TwitterApi } from 'twitter-api-v2';
 
 const twitterClient = new TwitterApi({
-  appKey: import.meta.env.VITE_TWITTER_API_KEY,
-  appSecret: import.meta.env.VITE_TWITTER_API_SECRET,
-  accessToken: import.meta.env.VITE_TWITTER_ACCESS_TOKEN,
-  accessSecret: import.meta.env.VITE_TWITTER_ACCESS_SECRET,
+  appKey: process.env.TWITTER_API_KEY || '',
+  appSecret: process.env.TWITTER_API_SECRET || '',
+  accessToken: process.env.TWITTER_ACCESS_TOKEN || '',
+  accessSecret: process.env.TWITTER_ACCESS_SECRET || '',
 });
 
 export const sendTweet = async (content: string): Promise<boolean> => {
